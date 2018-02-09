679ae3c2238248724ab6724a41ad7f1ca861af9d
PJ Frias <peejfrias@gmail.com>
2018-02-09 13:46:16 +0000

    set file structure and migrate tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/' do
+    "<h1>Sinatra NYC</h1>"
+  end
 end
---
+++ app/models/figure_title.rb
@@ -0,0 +1,4 @@
+class Figure <ActiveRecord::Base
+  belongs_to :figures
+  belongs_to :titles
+end
